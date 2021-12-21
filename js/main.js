e91320bf7d7dd6d500a8636f07321212167c167a
Chapalah <70241721+Chapalah@users.noreply.github.com>
2021-12-21 19:45:26 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const burger = document.querySelector(".burger");
 const menu = document.querySelector(".header__menu");
-const links = document.querySelectorAll('.header__link a')
+const links = document.querySelectorAll(".header__link a");
 
 burger.addEventListener("click", () => {
   burger.classList.toggle("burger_opened");
@@ -12,14 +12,15 @@ burger.addEventListener("click", () => {
   setTimeout(() => menu.classList.toggle("header__menu_show"), 150);
 });
 
-links.forEach(link => {
-  link.addEventListener('click', () => {
+links.forEach((link) => {
+  link.addEventListener("click", () => {
     if (burger.classList.contains("burger_opened")) {
-      menu.classList.remove("header__menu_show")
+      menu.classList.remove("header__menu_show");
       burger.classList.remove("burger_opened");
-    } 
-  })
-})
+      document.body.style.overflowY = "auto";
+    }
+  });
+});
 
 const aboutBlock = document.querySelector(".about");
 const mainBlock = document.querySelector(".main-block");
@@ -28,8 +29,6 @@ const roadmapBlock = document.querySelector(".roadmap");
 window.addEventListener("load", () => {
   isPartiallyVisible(mainBlock) && showTree();
 });
-
-showRoad()
 
 window.addEventListener(
   "scroll",
@@ -45,7 +44,7 @@ function showTree() {
   const tree = document.querySelector(".main-block__tree");
   const ground = document.querySelector(".main-block__ground");
 
-  setTimeout(() => tree.classList.add("main-block__tree_show"), 350);
+  tree.classList.add("main-block__tree_show");
   ground.classList.add("main-block__ground_show");
 }
 
@@ -64,34 +63,22 @@ function showRoad() {
     setTimeout(() => {
       road.classList.add("roadmap__item_show");
     }, ms);
+ 
+    setTimeout(() => {
+      road.classList.add("roadmap__item_way");
+    }, (ms + 600));
   });
 }
 
 function isPartiallyVisible(el) {
-  var elementBoundary = el.getBoundingClientRect();
+	var wt = $(window).scrollTop();
+	var wh = $(window).height();
+	var et = $(el).offset().top;
+	var eh = $(el).outerHeight();
+	var dh = $(document).height();   
 
-  var top = elementBoundary.top;
-  var bottom = elementBoundary.bottom + 450;
-  var height = elementBoundary.height;
-
-  return top + height >= 0 && height + window.innerHeight >= bottom;
+  return wt + wh >= et || wh + wt == dh || eh + et < wh;
 }
-
-// function isPartiallyVisible(el) {
-//   var elementBoundary = el.getBoundingClientRect();
-
-//   var top = $(elementBoundary).offset().top;
-//   var bottom = $(elementBoundary).offset().bottom + 150;
-//   var height = $(elementBoundary).height;
-
-//   return top + height >= 0 && height + window.innerHeight >= bottom;
-// }
-
-$(window).scroll(function() {
-  if ($(this).scrollTop() > 0 ) {
-    console.log($(this).scrollTop());
-  }
-});
 const trees = document.querySelector(".gallery__trees");
 
 let bgPosition = 10;
@@ -101,18 +88,6 @@ setInterval(() => {
 
 const logos = document.querySelectorAll('.company__logo')
 
-// links.forEach((link) => {
-//   link.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     const block = document.querySelector(`.${link.getAttribute("data-link")}`);
-
-//     block.scrollIntoView({
-//       behavior: "smooth",
-//       block: "start",
-//     });
-//   });
-// });
-
 $(links).on("click", function(e){
   e.preventDefault();
   var anchor = $(this).attr('href');
@@ -121,15 +96,10 @@ $(links).on("click", function(e){
   }, 800);
 });
 
-
-
-logos.forEach(logo => {
-   logo.addEventListener('click', (e) => {
-      e.preventDefault();
-
-      mainBlock.scrollIntoView({
-         behavior: "smooth",
-         block: "start",
-       });
-   })
-})
+$(logos).on("click", function(e){
+  e.preventDefault();
+  var anchor = $(this).attr('href');
+  $('html, body').stop().animate({
+      scrollTop: $(anchor).offset().top - 60
+  }, 800);
+});
